a094df0075f8cf2187a58880552fcb886451492d
Wouter <wtrvngl@gmail.com>
2022-10-31 18:49:02 +0100

    DbContext + EF Nuget Packages

--- Companions.API/Program.cs
+++ Companions.API/Program.cs
@@ -1,3 +1,6 @@
+using Companions.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer("Server=localhost;Database=Companions;Trusted_Connection=True;"));
 
 var app = builder.Build();
 
