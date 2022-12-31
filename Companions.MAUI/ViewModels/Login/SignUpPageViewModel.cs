a572ad6d204c48c2ab1b54713dac740f2433bf24
WTRVGL <wtrvngl@gmail.com>
2022-12-31 02:29:28 +0100

    Can sign up with Alerts when error

--- Companions.MAUI/ViewModels/Login/SignUpPageViewModel.cs
+++ Companions.MAUI/ViewModels/Login/SignUpPageViewModel.cs
@@ -21,61 +21,33 @@ namespace Companions.MAUI.ViewModels.Login
         public SignUpPageViewModel(IAuthService authService)
         {
             _authService = authService;
-
-            Email = new ValidatableObject<string>();
-            FirstName = new ValidatableObject<string>();
-
-
-            AddValidations();
         }
 
 
-        private void AddValidations()
-        {
 
-            Email.Validations.Add(new EmailRule<string>
-            {
-                ValidationMessage = "A valid email must be entered."
-            });
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _email;
 
-            FirstName.Validations.Add(new IsNotNullOrEmptyRule<string>
-            {
-                ValidationMessage = "First name is required."
-            });
-        }
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _firstName;
 
-        public ValidatableObject<string> Email { get; private set; }
-        public ValidatableObject<string> FirstName { get; private set; }
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _lastName;
 
-        [RelayCommand]
-        public void ValidateEmail()
-        {
-            Email.Validate();
-        }
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _password;
 
-        //[ObservableProperty]
-        //[NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
-        //private string _email;
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private string _passwordRepeat;
 
-        //[ObservableProperty]
-        //[NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
-        //private string _firstName;
-
-        //[ObservableProperty]
-        //[NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
-        //private string _lastName;
-
-        //[ObservableProperty]
-        //[NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
-        //private string _password;
-
-        //[ObservableProperty]
-        //[NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
-        //private string _passwordRepeat;
-
-        //[ObservableProperty]
-        //[NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
-        //private bool _TOSChecked;
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SignUpCommand))]
+        private bool _TOSChecked;
 
         [RelayCommand]
         async void GoToLogin()
@@ -86,47 +58,47 @@ namespace Companions.MAUI.ViewModels.Login
         [RelayCommand(CanExecute = nameof(CanSignUp))]
         async void SignUp()
         {
+            if (Password != PasswordRepeat)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", $"Passwords do not match", "Ok");
+                return;
+            }
 
-            //var req = new RegisterModel
-            //{
-            //    FirstName = FirstName,
-            //    LastName = LastName,
-            //    Password = Password,
-            //    Username = Email  
-            //};
+            var req = new RegisterModel
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Password = Password,
+                Username = Email
+            };
 
-            //RegistrationResponse registrationResponse = await _authService.RegisterUser(req);
+            RegistrationResponse registrationResponse = await _authService.RegisterUser(req);
 
-            //if (registrationResponse.RegistrationStatus == RegistrationStatus.UserExists)
-            //{
-            //    await Application.Current.MainPage.DisplayAlert("Error", $"User already exists", "Ok");
-            //    return;
-            //}
+            if (registrationResponse.RegistrationStatus == RegistrationStatus.UserExists)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", $"User already exists", "Ok");
+                return;
+            }
 
-            ////Retrieve JWT
-            //var loginModel = new LoginModel { Username = req.Username, Password = req.Password };
-            //var token = await _authService.GetJWTToken(loginModel);
+            //Retrieve JWT
+            var loginModel = new LoginModel { Username = req.Username, Password = req.Password };
+            var token = await _authService.GetJWTToken(loginModel);
 
-            ////Set token
-            //SecureStorage.SetAsync("JWT", token);
+            //Set token
+            await SecureStorage.SetAsync("JWT", token);
         }
 
         private bool CanSignUp()
         {
-            //if (
-            //    string.IsNullOrWhiteSpace(Email) ||
-            //    string.IsNullOrWhiteSpace(Password) ||
-            //    string.IsNullOrWhiteSpace(PasswordRepeat) ||
-            //    TOSChecked == false)
-            //{
-            //    return false;
-            //}
+            if (
+                string.IsNullOrWhiteSpace(Email) ||
+                string.IsNullOrWhiteSpace(Password) ||
+                string.IsNullOrWhiteSpace(PasswordRepeat) ||
+                TOSChecked == false)
+            {
+                return false;
+            }
 
-            //if (!string.Equals(Password, PasswordRepeat))
-            //{
-            //    return false;
-            //}
-            //else 
             return true;
         }
     }
