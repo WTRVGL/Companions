e669cff2b3a885e4ec7bdc022b7e9bd3dcfc9a11
WTRVGL <wtrvngl@gmail.com>
2023-01-05 00:42:05 +0100

    Stuff added

---
+++ Companions.MAUI/Shells/LoginShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Companions.MAUI.Views.App;
+using Companions.MAUI.Views.Login;
+
+namespace Companions.MAUI
+{
+    public partial class LoginShell : Shell
+    {
+        public LoginShell()
+        {
+            InitializeComponent();
+
+            //Register routing
+            Routing.RegisterRoute(nameof(LoginPage),typeof(LoginPage));
+            Routing.RegisterRoute(nameof(SignUpPage), typeof(SignUpPage));
+            Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+
+        }
+    }
+}
