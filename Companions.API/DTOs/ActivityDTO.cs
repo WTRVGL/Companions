c792f494305e73197a3261f58600e7d9a7de60b0
WTRVGL <wtrvngl@gmail.com>
2023-01-05 05:30:59 +0100

    Removed ImageURL from ActivityDTO in API

--- Companions.API/DTOs/ActivityDTO.cs
+++ Companions.API/DTOs/ActivityDTO.cs
@@ -5,7 +5,6 @@
         public string Id { get; set; }
         public ActivityTypeDTO ActivityType { get; set; }
         public string BuddyId { get; set; }
-        public string ImageURL { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
     }
